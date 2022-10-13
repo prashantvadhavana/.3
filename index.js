e1e5b400b6a49e040bd26580a8130314052246fd
prashantvadhavana <prashantvadhavana.vision@gmail.com>
2022-10-13 16:41:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:3000');
+});
